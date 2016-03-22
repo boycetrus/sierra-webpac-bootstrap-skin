c3254c5cb47274e8dc914dbe0b707fc56803ace0
Martin Boyce <boycetrus@gmail.com>
2016-03-22 11:57:59 +1100

    updated form javascript
    Remove all innovative javascript to see if form still works.
    re-write javascript to hide PIN field if the New PIN fieldset is active.

--- ipsso/ipsso.js
+++ ipsso/ipsso.js
@@ -1,29 +1,35 @@
 //Hide the main PIN entry if the new pin section is active.
-try { if ( document.getElementById("ipssonewpin") ) {
-	document.getElementById("ipssopinentry").style.display="none"; } }
-catch(err) {}
+// try { if ( document.getElementById("ipssonewpin") ) {
+// 	document.getElementById("ipssopinentry").style.display="none"; } }
+// catch(err) {}
 
 //Look for the first field in the external patron part of the form. This field will get cursor focus.
-var ipssoFirstField;
-try { ipssoFirstField = document.forms[0].extpatid; }
-catch(err) {
-}
+// var ipssoFirstField;
+// try { ipssoFirstField = document.forms[0].extpatid; }
+// catch(err) {
+// }
 //If we still don't have a field, look for the name field in the library account part.
-if ( ipssoFirstField===undefined ) { ipssoFirstField = document.forms[0].name; }
+// if ( ipssoFirstField===undefined ) { ipssoFirstField = document.forms[0].name; }
 //Set focus. Ignore errors.
-try { ipssoFirstField.focus(); }
-catch(err) {}
+// try { ipssoFirstField.focus(); }
+// catch(err) {}
 
-function enterSubmit(e) {
-var keycode;
-if (window.event) keycode = window.event.keyCode;
-else if (e) keycode = e.which;
-if (keycode===13)
- document.forms[0].submit();
-}
-
-document.onkeydown = enterSubmit;
+// function enterSubmit(e) {
+// var keycode;
+// if (window.event) keycode = window.event.keyCode;
+// else if (e) keycode = e.which;
+// if (keycode===13)
+//  document.forms[0].submit();
+// }
+//
+// document.onkeydown = enterSubmit;
 
 $(document).ready(function() {
   $('#status').addClass('alert alert-danger');
+
+  var newpinreq = $('#ipssonewpin');
+  if (newpinreq) {
+    $('#ipssopinentry').hide();
+  }
+
 });
