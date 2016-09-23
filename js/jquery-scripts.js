2529d3fe9b3f94b3557fdf243de8bb7e25142a1e
Martin Boyce <boycetrus@gmail.com>
2016-09-23 14:56:56 +1000

    clean up patfunc fines table

--- js/jquery-scripts.js
+++ js/jquery-scripts.js
@@ -110,9 +110,13 @@ $(document).ready(function() {
   $('.patfunc-fines .ecommerce-pay').parents('a').addClass('btn btn-primary').insertAfter('table.patfunc-fines');
   $('.patfunc-fines tr:first-child').remove();
   $('.patfunc-fines tr:last-child').remove();
-  // $('td.patFuncFinesEntryTitle').attr('colspan','3'); //temporary fix until move text to different td
-  // $('.patFuncFinesEntryDetail td:last-child').remove();
-  // $('.patFuncFinesTotal td:last-child').remove();
+  $('.patFuncFinesDetailDateLabel').removeAttr('colspan');
+  $('.patFuncFinesEntryDetail td:first-child').remove();
+  $('.patFuncFinesDetailDate td:first-child').remove();
+  $('.patFuncFinesTotalLabel').removeAttr('colspan');
+  $('td.patFuncFinesEntryTitle').attr('colspan','2')
+  $('.patFuncFinesEntryDetail td:last-child').remove();
+  $('.patFuncFinesTotal td:last-child').remove();
 
   $('form[name="mylists_form"] table').stacktable();
   $('form[name="PSEARCHFORM"] table').stacktable({
