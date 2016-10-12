4b6ad21105f49b24dc54e8b0d34d202fa07d9b9c
Martin Boyce <boycetrus@gmail.com>
2016-10-12 12:44:07 +1100

    modpinfo.html v4

--- js/modpinfo-scripts.js
+++ js/modpinfo-scripts.js
@@ -5,8 +5,7 @@ $(function() {
 
   $('.email input').attr('type','email').addClass('form-control');
   $('.phone input').attr('type','number').addClass('form-control');
-  $('#noticePref input').wrap('<label class="radio-inline"></label>');
-  $('#noticePref > br').remove();
+  $('#noticePref input').next('br').remove().wrap('<label class="radio-inline"></label>');
 
   $('.form-buttons a:first-child').addClass('btn btn-primary');
   $('.form-buttons a').not().eq(0).addClass('btn btn-default');
