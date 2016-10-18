dbcc6d021df04cbe3d42bf106765d9cf2dcb20c0
Martin Boyce <boycetrus@gmail.com>
2016-10-18 11:30:45 +1100

    finalise modpinfo.html

--- js/modpinfo-scripts.js
+++ js/modpinfo-scripts.js
@@ -18,10 +18,10 @@ $(function() {
 
   //toggle show/hide help-block text on input focus
   $('.editpinfo input').on('focus', function() {
-    $(this).parentsUntil('div').children('.help-block').removeClass('hidden');
+    $(this).closest('div').children('.help-block').removeClass('hidden');
   });
   $('.editpinfo input').on('blur', function() {
-    $(this).parentsUntil('div').children('.help-block').addClass('hidden');
+    $(this).closest('div').children('.help-block').addClass('hidden');
   });
 
 });
