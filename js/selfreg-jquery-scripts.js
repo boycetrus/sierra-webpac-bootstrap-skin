1ff76fe343716ab1376254dcd8ec0658efa57ed2
Martin Boyce <boycetrus@gmail.com>
2016-10-31 14:15:58 +1100

    added input types to selfreg.html via js

--- js/selfreg-jquery-scripts.js
+++ js/selfreg-jquery-scripts.js
@@ -1,3 +1,7 @@
 $(function() {
   $('#selfreg .form-group input').addClass('form-control');
+
+  $('#zemailaddr input').attr('type','email');
+  $('#tphone1 input').attr('type','tel');
+  $('#F051birthdate input').attr('type','date');
 });
