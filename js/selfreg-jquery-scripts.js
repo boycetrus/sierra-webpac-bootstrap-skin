650fc80fb91204f8d0a1f1fd3385cd0b1e9b5cac
Martin Boyce <boycetrus@gmail.com>
2016-11-04 11:59:03 +1100

    show/hide help text on form field focus

--- js/selfreg-jquery-scripts.js
+++ js/selfreg-jquery-scripts.js
@@ -4,4 +4,12 @@ $(function() {
   $('#zemailaddr').attr('type','email');
   $('#tphone1').attr('type','tel');
   $('#F051birthdate').attr('type','date').attr('max_length','10');
+
+  //toggle show/hide help-block text on input focus
+  $('.form-group input').on('focus', function() {
+    $(this).closest('div').children('.help-block').removeClass('hidden');
+  });
+  $('.form-group input').on('blur', function() {
+    $(this).closest('div').children('.help-block').addClass('hidden');
+  });
 });
