a0e74021ac689be2d639f85fd6bf31f15c5112e8
Martin Boyce <boycetrus@gmail.com>
2016-11-22 15:11:11 +1100

    briefcit.html progress
    try using a panel for each result in the list

--- js/search-results-scripts.js
+++ js/search-results-scripts.js
@@ -1,11 +1,6 @@
 $(function() {
 
-  $('table.bibItems').addClass('table table-responsive collapse');
-
-  $('.copy-status button').on('click', function() {
-    $(this).parents('.copy-status').find('table.bibItems').collapse('toggle');
-  });
-
-  $('.briefcitRequest > a').addClass('btn btn-primary');
+  $('table.bibItems').addClass('table table-responsive');
+  $('.briefcitRequest > a').addClass('btn btn-success');
 
 });
