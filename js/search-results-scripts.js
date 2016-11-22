0ecbb732ee273800090e32c03a68b1649082d75a
Martin Boyce <boycetrus@gmail.com>
2016-11-22 11:18:47 +1100

    briefcit.html progress
    adding bs classes and collapse behaviour to copy-status table

---
+++ js/search-results-scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  $('table.bibItems').addClass('table table-responsive collapse');
+  $('.briefcitRequest > a').addClass('btn btn-primary');
+
+  $('.copy-status button').on('click', function() {
+    $(this).parents('.copy-status').find('table.bibItems').collapse('toggle');
+  });
+
+});
