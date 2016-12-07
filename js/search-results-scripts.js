f0f3fe9065bdf524fd8531a4933c79fda12a1c81
Martin Boyce <boycetrus@gmail.com>
2016-12-07 14:00:38 +1100

    briefcit.html copies table
    remove the width attributes on the table cells

--- js/search-results-scripts.js
+++ js/search-results-scripts.js
@@ -9,11 +9,16 @@ $(function() {
   $('table.briefcit > tbody > tr:first-child').insertBefore(topPagination);
   $('table.briefcit > tbody').addClass('briefcit-list');
   $('table.briefcit tr.browseSuperEntry').addClass('page-header');
+  $('tr.bibItemsHeader th').removeAttr('width');
+  $('tr.bibItemsEntry td').removeAttr('width');
 
   //hide the copies table in each .briefcit-item and setup toggle to show/hide when table exists
   $('table.bibItems').parents('.briefcit-item').addClass('has-copy-table');
-  $('<a>Show Details</a>').appendTo('.briefcit-item .copy-status');
-  $('.copy-status a').on('click', function() {
+  $('<a>Show Details</a>').appendTo('.briefcit-item.has-copy-table .briefcit-status');
+  $('.briefcit-status a').on('click', function() {
     $(this).parents('.panel-body').next('.briefcit-copies').toggleClass('sr-only');
   });
+
+  // grab the tr that contains .breifcitAddlCopies and append it to the .bibItems table
+  // then remove the unnecessary table
 });
