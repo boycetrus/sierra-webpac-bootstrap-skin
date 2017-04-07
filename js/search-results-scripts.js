0926407ec0a5530343ef718cb178ba755868231d
Martin Boyce <boycetrus@gmail.com>
2017-04-07 11:54:33 +1000

    added css and js for new briefcit layout with jackets

--- js/search-results-scripts.js
+++ js/search-results-scripts.js
@@ -13,37 +13,37 @@ $(function() {
   $('tr.bibItemsEntry td').removeAttr('width');
   $('td.browseSaveJump').parent('tr').addClass('browse-jump');
 
-  //hide the copies table in each .briefcit-item and setup toggle to show/hide when table exists
-  $('table.bibItems').parents('.briefcit-item').addClass('has-copy-table');
-  $('<a>Show Copies</a>').appendTo('.briefcit-item.has-copy-table .briefcit-status');
-  $('.briefcit-status a').on('click', function() {
-    $(this).parents('.panel-body').next('.briefcit-copies').toggleClass('sr-only');
-  });
+  // //hide the copies table in each .briefcit-item and setup toggle to show/hide when table exists
+  // $('table.bibItems').parents('.briefcit-item').addClass('has-copy-table');
+  // $('<a>Show Copies</a>').appendTo('.briefcit-item.has-copy-table .briefcit-status');
+  // $('.briefcit-status a').on('click', function() {
+  //   $(this).parents('.panel-body').next('.briefcit-copies').toggleClass('sr-only');
+  // });
+  //
+  // //remove the copy status text for ebooks and eAudio
+  // $('.briefcit-item').not('.has-copy-table').find('.briefcit-status').remove();
+  //
+  // // grab .breifcitAddlCopies and append it to a new row in .bibItems table, then remove the extra table
+  // $('.to-full-record').parent('a').addClass('record-link');
+  // $('.bibItems > tbody').append('<tr><td colspan="3" class="extra-copies"></td></tr>');
 
-  //remove the copy status text for ebooks and eAudio
-  $('.briefcit-item').not('.has-copy-table').find('.briefcit-status').remove();
-
-  // grab .breifcitAddlCopies and append it to a new row in .bibItems table, then remove the extra table
-  $('.to-full-record').parent('a').addClass('record-link');
-  $('.bibItems > tbody').append('<tr><td colspan="3" class="extra-copies"></td></tr>');
-
-  $('.briefcit-copies').each(function() {
-    var $hasAddlCopies = $(this).children('table');
-    var $recordLink;
-
-    //find the .to-full-record link and move it to td.extra-copies
-    $recordLink = $(this).find('.record-link');
-    $(this).find('.extra-copies').append($recordLink);
-
-    //if there are more than three copies webpac inserts an extra table el with a note about extra copies
-    //move the extra copies text into td.extra-copies
-    if ($hasAddlCopies.length > 1) {
-      //grab the additional copies text and insert it into td.extra-copies before the .to-full-record link
-      var $addlCopiesText = $(this).find('.briefcitAddlCopies').text();
-      var $addlCopies = $addlCopiesText + ". ";
-      $(this).find('.extra-copies').prepend($addlCopies);
-      //remove the now surplus table element
-      $($hasAddlCopies[1]).remove();
-     }
-  });
+  // $('.briefcit-copies').each(function() {
+  //   var $hasAddlCopies = $(this).children('table');
+  //   var $recordLink;
+  //
+  //   //find the .to-full-record link and move it to td.extra-copies
+  //   $recordLink = $(this).find('.record-link');
+  //   $(this).find('.extra-copies').append($recordLink);
+  //
+  //   //if there are more than three copies webpac inserts an extra table el with a note about extra copies
+  //   //move the extra copies text into td.extra-copies
+  //   if ($hasAddlCopies.length > 1) {
+  //     //grab the additional copies text and insert it into td.extra-copies before the .to-full-record link
+  //     var $addlCopiesText = $(this).find('.briefcitAddlCopies').text();
+  //     var $addlCopies = $addlCopiesText + ". ";
+  //     $(this).find('.extra-copies').prepend($addlCopies);
+  //     //remove the now surplus table element
+  //     $($hasAddlCopies[1]).remove();
+  //    }
+  // });
 });
